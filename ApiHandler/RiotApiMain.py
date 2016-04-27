14d590c47c5fc7070f36d5b3350f75034b4fa067
ofekazar <ofekazaray3@gmail.com>
2016-04-27 19:09:52 +0300

    deleted python code, working derectory is now fixed

--- ApiHandler/RiotApiMain.py
+++
@@ -1,9 +0,0 @@
-from api_key import API_KEY as API_KEY
-from RiotApi import RiotApi
-
-def main():
-	api = RiotApi(API_KEY)
-	print api.get_summoner_id("eune", "bembelbi")
-
-if __name__ == "__main__":
-	main()
